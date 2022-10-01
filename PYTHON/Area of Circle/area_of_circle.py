04f9c95623cd45232972c3cd85bf40ddb9369881
Rajdeep Das <dasrajdeep0306@gmail.com>
2022-10-01 17:20:10 +0530

    Update and rename PYTHON/area_of_circle.py to PYTHON/Area of Circle/area_of_circle.py

--- PYTHON/Area of Circle/area_of_circle.py
+++ PYTHON/Area of Circle/area_of_circle.py
@@ -1,3 +1,9 @@
 from math import pi
 r = float(input ("Input the radius of the circle : "))
 print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
+
+
+# OUTPUT:
+
+# Input the radius of the circle : 3
+# The area of the circle with radius 3.0 is: 28.274333882308138
