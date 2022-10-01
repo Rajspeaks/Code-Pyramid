5870e7d87710f1c24548d97a193aeeec9556195b
Rajdeep Das <dasrajdeep0306@gmail.com>
2022-10-01 17:26:26 +0530

    Create Sum of Natural numbers using reursion.c

---
+++ C/Sum of Natural Numbers Using Recursion/Sum of Natural numbers using reursion.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int addNumbers(int n);
+
+int main() {
+
+  int num;
+  printf("Enter a positive integer: ");
+  scanf("%d", &num);
+  printf("Sum = %d", addNumbers(num));
+  return 0;
+}
+
+int addNumbers(int n) {
+  if (n != 0)
+    return n + addNumbers(n - 1);
+  else
+    return n;
+}
+
+
+// OUTPUT:
+// Enter a positive integer: 20
+// Sum = 210
