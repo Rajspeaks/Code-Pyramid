71263b599e26bd53b6b80064c56ff312a377f1d1
Octo Iron <85844307+octoiron@users.noreply.github.com>
2022-10-02 16:10:22 +0530

    Create Conversion.py

---
+++ PYTHON/Convert Decimal to Binary, Octal and Hexadecimal/Conversion.py
@@ -0,0 +1,13 @@
+# Python program to convert decimal into other number systems
+dec = 344
+
+print("The decimal value of", dec, "is:")
+print(bin(dec), "in binary.")
+print(oct(dec), "in octal.")
+print(hex(dec), "in hexadecimal.")
+
+# OUTPUT:
+# The decimal value of 344 is:
+# 0b101011000 in binary.
+# 0o530 in octal.
+# 0x158 in hexadecimal.
